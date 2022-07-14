cb64ffab7967ef2b2402a439e693d832803ee487
Allan <allan.sduarte@gmail.com>
2022-07-14 15:39:18 -0300

    feat: Add faker deps

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Johan.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:ex_machina, "~> 2.7.0", only: :test}
+      {:ex_machina, "~> 2.7.0", only: :test},
+      {:faker, "~> 0.17", only: :test}
     ]
   end
 
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 
 ExUnit.start()
+Faker.start()
 Ecto.Adapters.SQL.Sandbox.mode(Johan.Repo, :manual)
