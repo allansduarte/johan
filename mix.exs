e1eeef501345cf418c1708432abdd092c95c34e0
Allan <allan.sduarte@gmail.com>
2022-07-15 17:25:36 -0300

    chore: Add scrivener deps

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Johan.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:ex_machina, "~> 2.7.0", only: :test},
-      {:faker, "~> 0.17", only: :test}
+      {:faker, "~> 0.17", only: :test},
+      {:scrivener_ecto, "~> 2.0"}
     ]
   end
 
