4f2a9b160712ccffb543e38eac69c6c206185339
Allan <allan.sduarte@gmail.com>
2022-07-15 17:25:04 -0300

    chore: remove log from tests

--- config/test.exs
+++ config/test.exs
@@ -24,7 +24,7 @@ config :johan, JohanWeb.Endpoint,
 config :johan, Johan.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, backends: [], level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
